a5e452464f6ba02aad74ca9502f9da40406982be
claire <basketballclaire@gmail.com>
2016-02-22 22:48:23 -0800

    initial commit

---
+++ Grocerease/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Grocerease
+//
+//  Created by Claire on 2/22/16.
+//  Copyright © 2016 Claire Wang. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://jokes-ter.firebaseio.com/"
